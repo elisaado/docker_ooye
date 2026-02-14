d57cf5c137c2c1c89a5a70b4a1a51ba2551a4b5c
Eli Saado <eli5saado@gmail.com>
2026-02-14 22:53:39 +0100

    clean up entrypoint shell script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,53 +3,17 @@
 set -e
 
 # Make sure the database exists
-touch /data/ooye.db
+touch /app/db/ooye.db
 
-cat << EOF > config.js
-module.exports = {
-	discordToken: "${DISCORD_TOKEN}"
-}
-EOF
-
-admin_invite_line=""
-admin_invite_empty=""
-
-if [ -n "${ADMIN_INVITE}" ]; then
-  admin_invite_line="- '${ADMIN_INVITE}'"
+if [ -f /app/registration.yaml ]; then
+  echo "Here is your registration YAML:"
+  echo ""
+  cat /app/registration.yaml
+echo ""
 else
-  admin_invite_empty="[]"
+  echo "No registration.yaml mounted on /app/registration.yaml, exiting"
+  exit 1
 fi
-
-cat << EOF > registration.yaml
-id: de8c56117637cb5d9f4ac216f612dc2adb1de4c09ae8d13553f28c33a28147c7
-hs_token: ${HS_TOKEN}
-as_token: ${AS_TOKEN}
-url: ${URL}
-sender_localpart: ${NAMESPACE_PREFIX}_bridge_bot
-protocols:
-  - discord
-namespaces:
-  users:
-    - exclusive: true
-      regex: '@${NAMESPACE_PREFIX}.*'
-  aliases:
-    - exclusive: true
-      regex: '#${NAMESPACE_PREFIX}.*'
-rate_limited: false
-ooye:
-  namespace_prefix: ${NAMESPACE_PREFIX}
-  max_file_size: 5000000
-  server_name: ${SERVER_NAME}
-  server_origin: ${SERVER_ORIGIN}
-  content_length_workaround: ${CONTENT_LENGTH_WORKAROUND}
-  invite: ${admin_invite_empty}
-    ${admin_invite_line}
-EOF
-
-echo "Here is your registration YAML:"
-echo ""
-cat registration.yaml
-echo ""
 
 echo "Setting up / seeding database if necessary"
 
